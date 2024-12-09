561fe67217a1a3cfa55f107ee3972270439e7d57
Mikhail Chernyshenko <chernyshenkomd@intech.rshb.ru>
2024-12-10 01:58:10 +0300

    feat(*): complete task 2.2

--- 2/2/index.mjs
+++ 2/2/index.mjs
@@ -1,40 +1,57 @@
 import readline from 'node:readline';
 import { createReadStream, createWriteStream } from 'node:fs';
-import path from 'node:path'
+import path from 'node:path';
 
 
 const inputStream = createReadStream(path.join(import.meta.dirname, './input.txt'));
 
 const input = readline.createInterface({ input: inputStream })
 
-const leftColumn = [];
-const rightColumn = {};
+let successReportsCount = 0;
 
 input.on('line', (line) => {
-  const [leftValue, rightValue] = line.trim().split(/\s+/)
+  const reportLevels = line.trim().split(/\s+/).map(Number)
 
-  leftColumn.push(leftValue);
-  rightColumn[rightValue] = rightColumn[rightValue] ? rightColumn[rightValue] + 1 : 1
+  const checkReports = (reports) => {
+    const direction = reports[0] > reports[1] ? -1 : 1;
+    for (let i = 1; i < reports.length; i++) {
+      const diff = (reports[i] - reports[i - 1]) * direction;
+      if (diff < 1 || diff > 3) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  const isValidReports = checkReports(reportLevels);
+
+  if (isValidReports) {
+    successReportsCount += 1;
+    return;
+  }
+
+  for (let i = 0; i < reportLevels.length; i++) {
+    const reportsCopy = reportLevels.slice();
+
+    reportsCopy.splice(i, 1);
+
+    const isValidReports = checkReports(reportsCopy);
+
+    if (isValidReports) {
+      successReportsCount += 1;
+      return;
+    }
+  }
+
 })
 
 
 input.on('close', () => {
   inputStream.close();
 
-  leftColumn.sort();
-
-  let result = 0;
-
-  for (let i = 0; i < leftColumn.length; i++) {
-    const leftValue = leftColumn[i];
-
-    const similarityScore = leftValue * (rightColumn[leftValue] ?? 0)
-
-    result += similarityScore;
-  }
-
   const outputStream = createWriteStream(path.join(import.meta.dirname, './output.txt'));
-  outputStream.write(result.toString(), (error) => {
+  outputStream.write(successReportsCount.toString(), (error) => {
     if (error) {
       console.log(error);
     }
